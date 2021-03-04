a1ebfabc8cfb6a66328563446ad32b3611ccb527
HamanCheese <79953650+HamanCheese@users.noreply.github.com>
2021-03-04 23:18:23 +1300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿
+using System;
+
+namespace Project_Charlie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtmobile_required = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label14 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.ErrorMessage = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Visitor Details";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(372, 101);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sign In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker.Location = new System.Drawing.Point(434, 112);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker.TabIndex = 2;
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(108, 83);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(273, 26);
+            this.txtFname.TabIndex = 5;
+            this.txtFname.TextChanged += new System.EventHandler(this.txtFname_required_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Last Name";
+            // 
+            // txtLname
+            // 
+            this.txtLname.Location = new System.Drawing.Point(108, 128);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(271, 26);
+            this.txtLname.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(430, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Mobile";
+            // 
+            // txtmobile_required
+            // 
+            this.txtmobile_required.Location = new System.Drawing.Point(108, 176);
+            this.txtmobile_required.Name = "txtmobile_required";
+            this.txtmobile_required.Size = new System.Drawing.Size(271, 26);
+            this.txtmobile_required.TabIndex = 17;
+            this.txtmobile_required.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtmobile_required_KeyPress);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(430, 23);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(111, 20);
+            this.label10.TabIndex = 33;
+            this.label10.Text = "Meeting Detail";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(770, 23);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(113, 20);
+            this.label11.TabIndex = 34;
+            this.label11.Text = "Visitors on-site";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(8, 220);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 20);
+            this.label12.TabIndex = 35;
+            this.label12.Text = "Email\r\n";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(501, 203);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(43, 20);
+            this.label13.TabIndex = 37;
+            this.label13.Text = "Time";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(434, 226);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 39;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(574, 226);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown2.TabIndex = 40;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(430, 285);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(102, 20);
+            this.label14.TabIndex = 41;
+            this.label14.Text = "Meeting With";
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Des Troy",
+            "Buf Ering",
+            "Oot Reb"});
+            this.comboBox.Location = new System.Drawing.Point(434, 332);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(174, 28);
+            this.comboBox.TabIndex = 42;
+            this.comboBox.Text = "Please Choose Any";
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(430, 393);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 20);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "Meeting Aim";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(434, 434);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 29);
+            this.button2.TabIndex = 44;
+            this.button2.Text = "Site Visit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(774, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(357, 564);
+            this.listBox1.TabIndex = 45;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(560, 228);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 20);
+            this.label7.TabIndex = 46;
+            this.label7.Text = ":";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.txtemail);
+            this.panel1.Controls.Add(this.ErrorMessage);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.comboBox);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.txtmobile_required);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtLname);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtFname);
+            this.panel1.Controls.Add(this.dateTimePicker);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1182, 702);
+            this.panel1.TabIndex = 47;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(434, 489);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 44);
+            this.button3.TabIndex = 49;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(108, 226);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(271, 26);
+            this.txtemail.TabIndex = 48;
+            // 
+            // ErrorMessage
+            // 
+            this.ErrorMessage.AutoSize = true;
+            this.ErrorMessage.Location = new System.Drawing.Point(8, 285);
+            this.ErrorMessage.Name = "ErrorMessage";
+            this.ErrorMessage.Size = new System.Drawing.Size(252, 20);
+            this.ErrorMessage.TabIndex = 47;
+            this.ErrorMessage.Text = "Please Fill In all the required Areas";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(541, 489);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 44);
+            this.button4.TabIndex = 50;
+            this.button4.Text = "Clear ListBox";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 787);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void txtemail_required_TextChanged(object sender, EventArgs e) => throw new NotImplementedException();
+         
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtmobile_required;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label ErrorMessage;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace Project_Charlie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool buttonwasclicked = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctl in panel1.Controls)
+            {
+                if (ctl is ComboBox)
+                        
+                {
+                    int find = ctl.Name.ToString().IndexOf("_required", 0);
+                    if (find > 0 && ctl.Text == "")
+                    {
+                        ctl.BackColor = Color.Red;
+                        ErrorMessage.Text = "Mandatory Field Should Be filled";
+                        ErrorMessage.ForeColor = Color.Red;
+                        ErrorMessage.Visible = true;
+                    }
+                    else
+                    {
+
+                        ctl.BackColor = Color.White;
+                        ErrorMessage.Text = "Please Fill In All The required Areas";
+                        ErrorMessage.ForeColor = Color.Green;
+                        ErrorMessage.Visible = true;
+
+                    }
+                    {
+                        if (ValidateChildren(ValidationConstraints.Enabled))
+                        {
+                            MessageBox.Show("Missing", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else if (buttonwasclicked == false)
+                         {
+                            MessageBox.Show("Please Click The Button");
+                        }
+                        
+
+                            listBox1.Items.Add("Name: " + txtFname.Text + " " + txtLname.Text);
+                            listBox1.Items.Add("Mobile Number: " + txtmobile_required.Text);
+                            listBox1.Items.Add("Hours: " + numericUpDown1.Value + " " + numericUpDown2.Value);
+                            listBox1.Items.Add("Meeting With: " + comboBox.SelectedItem);
+                            listBox1.Items.Add("Date: " + dateTimePicker.Value);
+                        
+                    }
+                }
+            }
+
+
+
+        }
+
+
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+
+            if (f2.ShowDialog() == DialogResult.OK)
+            {
+                if (f2.radioButton1.Checked == true)
+                    button2.Text = f2.radioButton1.Text;
+                else if (f2.radioButton2.Checked == true)
+                    button2.Text = f2.radioButton2.Text;
+                else if (f2.radioButton3.Checked == true)
+                    button2.Text = f2.radioButton3.Text;
+                else if (f2.radioButton4.Checked == true)
+                    button2.Text = f2.radioButton4.Text;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFname_required_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+       
+        private void txtfname_required_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+                char chr = e.KeyChar;
+                if (!char.IsLetter(chr) && chr != 8)
+                {
+                    e.Handled = true;
+                    MessageBox.Show("Please Enter A vaild Name");
+                }
+            
+        }
+        private void txtlname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            char chr = e.KeyChar;
+            if (!char.IsLetter(chr) && chr != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show("Please Enter A vaild Name");
+            }
+
+        }
+
+        private void txtmobile_required_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char chr = e.KeyChar;
+            if (!char.IsDigit(chr) && chr != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show("Please Enter A vaild Mobile Number");
+            }
+
+        }
+       
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtFname.Clear();
+            txtLname.Clear();
+            txtmobile_required.Clear();
+            txtemail.Clear();
+            numericUpDown1.Value = 0;
+            numericUpDown2.Value = 0;
+            comboBox.Text = "Select Any";
+            button2.Text = "Meeting Aim";
+            dateTimePicker.ResetText();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove("Name: " + txtFname.Text + " " + txtLname.Text);
+            listBox1.Items.Remove("Mobile Number: " + txtmobile_required.Text);
+            listBox1.Items.Remove("Hours: " + numericUpDown1.Value + " " + numericUpDown2.Value);
+            listBox1.Items.Remove("Meeting With: " + comboBox.SelectedItem);
+            listBox1.Items.Remove("Date: " + dateTimePicker.Value);
+        }
+
+        //private void txtemail_required_Leave(object sender, EventArgs e)
+        //{
+        //string pattern = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+        //if (Regex.IsMatch(txtemail_required.Text,pattern))
+
+        //{
+        //errorProvider.Clear();
+    }
+            //else
+            //{
+                //errorProvider.SetError(this.txtemail_required, "Please Provide A Valid Email Adress");
+               // return;
+            }
+//}
+//}
+//}
+
+
+
+
+//notes
+//if(textbox1.text=="")
+//{ messagebox.show("Fisrt Name");
+//}
+//textbox1.backcolr=color.red;
+//textbox1.focus();
+//else if (int.tryparse(textbox2.text,out num)==false) {textbox1.backcolor = color.white;}
+//int num = 0;
+//messagebox.show("Enter Digits Only");
+//textbox2.backcolr=color.green;
+//else if
+//{textbox2.backcolr==colr.blue;}
+//else if numeric updawn.value=0
+//messagebox.show("Please enter the hour time");
+
+//important
+
+//else if(combobox1.selectedIndex==-1)
+//{messagebox.show("Please select any item");}
+
+
+//buttonwasclicked = true;
+//else if (buttonwasclicked == false)
+//messagebox.show("Please click the button");
+//
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
